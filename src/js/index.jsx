ff7ed6453a90d632120161f43c50d91220a59d72
Heng Ly Aun <aunhengly@gmail.com>
2018-05-03 20:00:20 -0700

    first intial

---
+++ src/js/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { render } from 'react-dom';
+import App from './app';
+import store from './rootStore';
+
+render(
+    <Provider store={ store }>
+        <App />
+    </Provider>,
+    document.getElementById('root')
+);
